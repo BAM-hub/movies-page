a49597b02a3b411654af028a0a281b01e06c3e6c
BAM-hub <bsharamin12@gmail.com>
2022-12-11 23:26:26 +0300

    added redux persist to retain state on refresh

--- src/store.js
+++ src/store.js
@@ -1,10 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./reducers/auth";
 import MovieReducer from "./reducers/movies";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     auth: AuthReducer,
     movie: MovieReducer,
-  },
+  })
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
