be15d4c9e8d89b8627a813a8657e2f350d54f0c7
BAM-hub <bsharamin12@gmail.com>
2022-12-12 22:15:49 +0300

    exported constants to be used in the app

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,8 @@
 // but we dont have a server secure our key, I will do it this way
 
 export const API_KEY = "8316be82a3fedf91e83245534d306be3";
-export const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+export const API_PREFIX = "https://api.themoviedb.org/";
+export const API_URL = `${API_PREFIX}3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+export const emailError = "please enter a valid email address";
+export const passwordError =
+  "password must contain 8-15 characters, at least one uppercase letter, one lowercase letter, one number and one special character";
